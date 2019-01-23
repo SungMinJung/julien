9522aa660e65cb3557f44c21cfa00cd85e26484a
phkhyun <phkhyun@daum.net>
2019-01-23 20:36:19 +0900

    코리아맵 완성

---
+++ resources/views/store/find.blade.php
@@ -0,0 +1,32 @@
+@extends('store.common')
+
+@section('inner')
+    <div class="row">
+        <div class="col-md-6">
+        </div>
+        
+        <div class="col-md-6">
+            <div class="map" id="korea">
+                <div class="busan"><p class="p-busan">부산</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/busan.png"  ></div>
+                <div class="gangwon"><p class="p-gangwon">강원도</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/gangwon.png"></div>
+                <div class="chungbok"><p class="p-chungbok">충청 북도</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/chungbuk.png" ></div>
+                <div class="chungnam"><p class="p-chungnam">충청 남도</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/chungnam.png" > </div>
+                <div class="daegu"><p class="p-daegu">대구</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/daegu.png" >    </div>
+                <div class="daejeon"><p class="p-daejeon">대전</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/daejeon.png">  </div>
+                <div class="gwangju"><p class="p-gwangju">광주</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/gwangju.png" >      </div>
+                <div class="gyeongbuk"><p class="p-gyeongbuk">경상 북도</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/gyeongbuk.png" ></div>
+                <div class="gyeonggi"><p class="p-gyeonggi">경기도</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/gyeonggi.png" ></div>
+                <div class="gyeongnam"><p class="p-gyeongnam">경상 남도</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/gyeongnam.png" ></div>
+                <div class="incheon"><p class="p-incheon">인천</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/incheon.png" ></div>
+                <div class="jeju"><p class="p-jeju">제주</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/jeju.png"></div>
+                <div class="jeonbuk"><p class="p-jeonbuk">전라 북도</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/jeonbuk.png" ></div>
+                <div class="jeonnam"><p class="p-jeonnam">전라 남도</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/jeonnam.png" ></div>
+                <div class="sejong"><p class="p-sejong">세종</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/sejong.png"></div>
+                <div class="seoul"><p class="p-seoul">서울</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/seoul.png"></div>
+                <div class="ulsan"><p class="p-ulsan">울산</p><img src="http://julienwaffle.com/wp-content/plugins/bbs-e-franchise/images/ulsan.png" ></div>
+
+            </div>
+        </div>
+    </div>
+@endsection
+
